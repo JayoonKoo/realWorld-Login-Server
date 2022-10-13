c1907afa72e5149f62fcd674c6df01910feb9253
jayoonkoo <rnwkdbs12@gmail.com>
2022-10-14 00:00:06 +0900

    1014 passport login

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+
+const postRouter = require("./routes/post.js");
+const userRouter = require("./routes/user.js");
+const db = require("./models");
+const passportConfig = require("./passport");
+const passport = require("passport");
+
+dotenv.config();
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("db 연결 성공");
+  })
+  .catch(console.error);
+passportConfig();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/post", postRouter);
+app.use("/user", userRouter);
+
+app.listen(3065, () => {
+  console.log("서버시작");
+});
---
+++ models/index.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.Comment = require("./comment.js")(sequelize, Sequelize);
+db.Hashtag = require("./hashtag.js")(sequelize, Sequelize);
+db.Image = require("./image.js")(sequelize, Sequelize);
+db.Post = require("./post.js")(sequelize, Sequelize);
+db.User = require("./user.js")(sequelize, Sequelize);
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
